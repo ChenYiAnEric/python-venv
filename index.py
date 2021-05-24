9126dc94613e2500147084526c8199a61b14ba37
DESKTOP-Eric <mopack80684@gmail.com>
2021-05-24 17:15:36 +0800

    新增靜態網頁

--- index.py
+++ index.py
@@ -1,24 +1,34 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask import request
+import requests
+import json
+from bs4 import BeautifulSoup
 
-
-app = Flask(__name__)
+app = Flask(__name__, template_folder='main')
 
 
 @app.route('/')
 def home():
-    return 'Home Page Route'
+    return render_template('index.html')
 
+@app.route("/api/power", methods=['GET'])
+def power():
+  searchText = request.args.get("s")
+  site = "https://www.coolpc.com.tw/eachview.php?IGrp=15"
+  response = requests.get(site)
+  soup = BeautifulSoup(response.text, "html.parser")
+  titles = soup.find_all("div", class_="t")
+  searchdata = []
+  for title in titles:
+    str = title.getText()
+    try:
+      sin = str.find(searchText)
+    except:
+      sin = -1
+    if sin != -1:
+      searchdata.append({'title': str})
+  jsonChang = json.dumps({'data': searchdata}, indent = 4)
+  return jsonChang
 
-@app.route('/about')
-def about():
-    return 'About Page Route'
-
-
-@app.route('/portfolio')
-def portfolio():
-    return 'Portfolio Page Route'
-
-
-@app.route('/contact')
-def contact():
-    return 'Contact Page Route'
+  if __name__ == '__main__':
+    app.run()
